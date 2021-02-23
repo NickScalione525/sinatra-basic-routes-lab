f6be7870dec47d53ffeb54c13ab1761e58205a6a
nick <nicholas_scalione@yahoo.com>
2021-02-23 01:02:18 -0500

    commit tree 869f6059133538e22845c81812e7b67b41f45133

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+     "My name is Nick"
+      end 
+
+
+      get '/hometown' do
+        "My hometown is Liverpool, NY!"
+      end 
+
+      get '/favorite-song' do
+        "My favorite song is The Wreck of the Edmund Fitgerald"
+      end 
 end
